acf71fbaec67f825ef306c6139e3c55d5f71ccb5
Soumyadeep081 <soumyadeep323@gmail.com>
2026-07-03 02:23:39 +0530

    Updated booking controller

--- src/main/java/org/example/uberprojectlocationservice/controllers/LocationController.java
+++ src/main/java/org/example/uberprojectlocationservice/controllers/LocationController.java
@@ -37,7 +37,7 @@ public class LocationController {
         }
     }
 
-    @GetMapping("/nearby/drivers")
+    @PostMapping("/nearby/drivers")
     public ResponseEntity<List<DriverLocationDto>> getNearbyDrivers(@RequestBody NearByDriversRequestDto nearByDriversRequestDto){
         try {
 
